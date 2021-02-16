f789b9b383b215c5056cbd2be591207735fd3534
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-02-16 12:37:57 +0300

    Makes blog default component and turn off the docs since we don't need it right now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,14 +84,11 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
+        docs: false,
         blog: {
           showReadingTime: true,
+          path: 'blog',
+          routeBasePath: '/',
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/blog/',
