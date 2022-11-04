7bb2fc5d16f5f4dbbd62fa893e6563ca61937b0f
Anthony Ilinykh <anthonyilinykh@gmail.com>
2022-11-04 15:27:04 +0400

    yarn upgrade @docusaurus/core@latest @docusaurus/preset-classic@latest

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,47 +1,46 @@
 module.exports = {
-  title: 'ailinykh.com',
-  tagline: 'personal blog',
-  url: 'https://ailinykh.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'ailinykh', // Usually your GitHub org/user name.
-  projectName: 'blog', // Usually your repo name.
+  title: "ailinykh.com",
+  tagline: "personal blog",
+  url: "https://ailinykh.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "ailinykh", // Usually your GitHub org/user name.
+  projectName: "blog", // Usually your repo name.
   themeConfig: {
     prism: {
-      additionalLanguages: ['swift'],
+      additionalLanguages: ["swift"],
     },
     navbar: {
-      title: 'Developer Notes',
+      title: "Developer Notes",
       logo: {
-        alt: 'Logo',
-        src: 'img/logo.svg',
-      }
+        alt: "Logo",
+        src: "img/logo.svg",
+      },
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} ailinykh.com`,
-    },
-    gtag: {
-      trackingID: 'G-VPL1442NPW',
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: false,
         blog: {
           showReadingTime: true,
-          path: 'blog',
-          routeBasePath: '/',
+          path: "blog",
+          routeBasePath: "/",
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/ailinykh/blog/edit/master/',
+          editUrl: "https://github.com/ailinykh/blog/edit/master/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        gtag: {
+          trackingID: "G-VPL1442NPW",
         },
       },
     ],
